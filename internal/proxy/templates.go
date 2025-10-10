c0390273d7ca0c4826f9338e2afbb18afecac7a6
Noah <117038300+NoahCxrest@users.noreply.github.com>
2025-10-10 11:41:54 -0600

    fix template

--- internal/proxy/templates.go
+++ internal/proxy/templates.go
@@ -8,4 +8,15 @@ import (
 //go:embed dashboard.html
 var dashboardFS embed.FS
 
-var dashboardTemplate = template.Must(template.New("dashboard").ParseFS(dashboardFS, "dashboard.html"))
+var dashboardTemplate *template.Template
+
+func init() {
+	data, err := dashboardFS.ReadFile("dashboard.html")
+	if err != nil {
+		panic(err)
+	}
+	dashboardTemplate, err = template.New("dashboard").Parse(string(data))
+	if err != nil {
+		panic(err)
+	}
+}
